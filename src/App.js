b39b85e51c11c4677c9479365303a15cabf123e8
Nouman3307 <66219900+Nouman3307@users.noreply.github.com>
2020-06-13 15:21:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  
+  let [isMorning, setMorning] = useState(false)
 
   return (
-    <div className={'box'}>
+    <div className={`-box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
       <Class4  counter={count}/>
       <button onClick={() => setCount(count + 3)}>
         Click me
