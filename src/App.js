23e055052dddc58b6ef159215702931422096a91
Nouman3307 <66219900+Nouman3307@users.noreply.github.com>
2020-06-13 12:29:24 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react';
+import {Class4} from './class4';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  let [ismorning, setday] =useState(true);
+
+  return (
+    <div className={'box'}>
+      <Class4  counter={count}/>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/class4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Class4(props){
+    return(
+    <h1>Count presed {props.counter} times</h1>
+    )
+}
